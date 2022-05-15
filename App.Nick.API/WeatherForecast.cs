869cb9bfa692e9271c847c72abd951b64603e374
nhucnguyen1992 <nick.nguyen@siliconstack.com.au>
2022-05-15 17:26:11 +0700

    first commit

---
+++ App.Nick.API/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace App.Nick.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+    //2648 + 3928 + 5508
+}
